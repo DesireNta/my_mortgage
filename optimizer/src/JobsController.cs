88c082285278486f16066c9dcabe598d91d0a773
NTAMBIYE Désiré <ntambiye.desire@gmail.com>
2020-03-24 16:59:53 +0100

    ADD : OPTIMIZER OK
    The optimizer test ended with success

---
+++ optimizer/src/JobsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Optimizer
+{
+    [Route("api/[controller]")]
+    public class JobsController : Controller
+    {
+        // Faire mieux que des static partagés
+        static internal object Verrou = new object();
+
+        static internal List<Job> Jobs = new List<Job>();
+
+        // GET: api/jobs
+        [HttpGet]
+        public IEnumerable<Job> GetAll()
+        {
+            return Jobs;
+        }
+
+        // PUT: api/jobs/id
+        [HttpPut("{id}")]
+        public void Put(string Id, [FromBody] Job value)
+        {
+            lock (Verrou)
+            {
+                Jobs.RemoveAll(j => j.Id == Id);
+                Jobs.Add(value);
+            }
+        }
+    }
+}
